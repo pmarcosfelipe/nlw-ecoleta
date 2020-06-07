93fafd8df4fd7740c8794b53ab76f1323e6fd7d4
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-06-06 23:07:24 -0300

    Configured Router of application

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+const CreatePoint = () => {
+  return <h1>Hello</h1>;
+};
+
+export default CreatePoint;
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
@@ -19,12 +21,12 @@ const Home = () => {
             Ajudamos pessoas a encontrarem pontos de coleta de forma eficiênte
           </p>
 
-          <a href="/cadastro">
+          <Link to="/create-point">
             <span>
               <FiLogIn />
             </span>
             <strong>Cadastre um ponto de coleta</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>

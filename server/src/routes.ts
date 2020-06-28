3924a4141b8ff9c53c459a6898d1c1b67042e07e
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-06-28 19:14:37 -0300

    Creating data validator using CELEBRATE

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { celebrate, Joi } from 'celebrate';
+
 import multer from 'multer';
 import multerConfig from './config/multer';
 
@@ -15,6 +17,25 @@ routes.get('/items', itemController.list);
 routes.get('/points', pointController.list);
 routes.get('/points/:id', pointController.show);
 
-routes.post('/points', upload.single('image'), pointController.create);
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    { abortEarly: false }
+  ),
+  pointController.create
+);
 
 export default routes;
